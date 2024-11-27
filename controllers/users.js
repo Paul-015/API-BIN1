e76e44aed1ea6aaeb29f9e13fc09f33b674d2207
Paul-015 <paul.bruneton@edu.devinci.fr>
2024-11-27 16:36:03 +0100

    reworked project architecture

---
+++ controllers/users.js
@@ -0,0 +1,20 @@
+const User = require('../models/user');
+
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json({ error: 'echec pour récuperer le user' });
+  }
+};
+
+exports.createUser = async (req, res) => {
+  try {
+    const newUser = new User(req.body);
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(400).json({ error: 'echec pour créer le user' });
+  }
+};
